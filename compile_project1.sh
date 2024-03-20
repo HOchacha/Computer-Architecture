0ccf63ae728c03284167c84ef2d306c7cf19fda1
HOchacha <outcider112@dankook.ac.kr>
2024-03-20 13:40:45 +0900

    chore: update Script

--- compile_project1.sh
+++ compile_project1.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if [ ! -d "Project1/bin" ]; then
-  mkdir Project1/bin
+# Check if the bin directory exists under Project1
+if [ ! -d "bin" ]; then
+  # If not, create the bin directory
+  mkdir bin
 fi
 
+# Run CMake to compile the project
 cmake -S . -B cmake-build-debug
 
+# Build the project
 cmake --build cmake-build-debug --config Debug
 
-mv cmake-build-debug/Computer_Architecture Project1/bin/
+# Check if a name for the binary was provided
+if [ -z "$1" ]; then
+  echo "No binary name provided. Using default: Project1"
+  binary_name="Project1"
+else
+  binary_name="$1"
+fi
+
+# Move the executable to the bin directory
+mv "cmake-build-debug/$binary_name" Project1/bin/
